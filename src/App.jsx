c6d4a227f4f6b26cf268df93d794651f88bf3630
yinyoude <39619762+yinyoude@users.noreply.github.com>
2023-05-15 15:35:25 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import {
+  closestCenter,
+  DndContext,
+  DragOverlay,
+  KeyboardSensor,
+  PointerSensor,
+  TouchSensor,
+  useSensor,
+  useSensors
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy
+} from "@dnd-kit/sortable";
+
+import { SortableItem } from "./SortableItem";
+import { Item } from "./Item";
+
+export default function App() {
+  const [activeId, setActiveId] = useState(null);
+  const [items, setItems] = useState(["1", "2", "3"]);
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates
+    }),
+    useSensor(TouchSensor)
+  );
+
+  return (
+    <DndContext
+      sensors={sensors}
+      collisionDetection={closestCenter}
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+    >
+      <SortableContext items={items} strategy={verticalListSortingStrategy}>
+        {items.map((id) => (
+          <SortableItem key={id} id={id} />
+        ))}
+      </SortableContext>
+    </DndContext>
+  );
+
+  function handleDragStart(event) {
+    const { active } = event;
+
+    setActiveId(active.id);
+  }
+
+  function handleDragEnd(event) {
+    const { active, over } = event;
+
+    if (active.id !== over.id) {
+      setItems((items) => {
+        const oldIndex = items.indexOf(active.id);
+        const newIndex = items.indexOf(over.id);
+
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+
+    setActiveId(null);
+  }
+}
---
+++ src/SortableItem.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {useSortable} from '@dnd-kit/sortable';
+import {CSS} from '@dnd-kit/utilities';
+
+import {Item} from './Item';
+
+export function SortableItem(props) {
+  const {
+    isDragging,
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+  } = useSortable({id: props.id});
+
+  const backgroundColor =
+    props.id == 1 ? 'Orange' : props.id == 2 ? 'pink' : 'gray';
+
+  const style = {
+    position: 'relative',
+    zIndex: isDragging ? 1 : undefined,
+    transform: CSS.Transform.toString(transform),
+    transition,
+    height: 100,
+    width: 100,
+    border: '2px solid black',
+    backgroundColor,
+    borderRadius: 10,
+    touchAction: 'none',
+    margin: 20,
+  };
+
+  return <Item ref={setNodeRef} style={style} {...attributes} {...listeners} />;
+}
